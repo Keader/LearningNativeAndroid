b5cd6efecdf129b591c964c20add04b04397559f
Keader <keader.android@gmail.com>
2020-09-25 10:17:50 -0300

    animation and other stuffs

--- 4_TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 4_TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -36,7 +36,7 @@ class TitleFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item!!,
+        return NavigationUI.onNavDestinationSelected(item,
                 view!!.findNavController())
                 || super.onOptionsItemSelected(item)
     }
